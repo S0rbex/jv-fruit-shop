419fe66f105028d3b969a63b84ab985ff22a9d81
S0rbex <vitalikmartunushun@gmail.com>
2025-12-24 02:01:54 +0200

    Fix checkstyle

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -27,7 +27,6 @@ public class Main {
 
     public static void main(String[] arg) {
 
-
         FileReader fileReader = new FileReaderImpl();
         List<String> inputReport = fileReader.read(FILE_PATH_FROM);
 
